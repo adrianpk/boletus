52736c86a6b8085da160b4e36bce49bd9485dedf
Adrian K <adrian.git@kuguar.dev>
2020-01-15 01:32:33 +0100

    Ticketer gRPC server.

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-protoc --proto_path=internal/app/grpc/api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/grpc/api/v1 api.proto
+protoc --proto_path=pkg/grpc/api/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/grpc/api/v1 api.proto
